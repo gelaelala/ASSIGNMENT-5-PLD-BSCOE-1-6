c2dd6eea9bda1c8b626d530ba7b474b783cb9b09
unknown <angelaescariocorpuz@gmail.com>
2021-11-16 13:28:32 +0800

    insertion of teen stage

--- program 3.py
+++ program 3.py
@@ -3,4 +3,7 @@ Age = int(input("Enter age: "))
 if Age > -1 and Age <= 12:
     print ("Kid")
 else:
-    print ("Others")
+    if Age >= 13 and Age <= 17:
+        print ("Teen")
+    else:
+        print ("Others")
