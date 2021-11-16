6e704ab9141e28c73abf6b5a31e6232c413c01bc
unknown <angelaescariocorpuz@gmail.com>
2021-11-16 11:39:40 +0800

    insertion of 1.5, 1.75, and 2.0 mark

--- program 1.py
+++ program 1.py
@@ -1,11 +1,23 @@
 grade = float(input("Enter grade: "))
 
 if grade >= 96.5 and grade <= 100.0:
-    print(1.0)
-    print ("Excellent")
+    print("Your grade is: 1.0")
+    print ("Description: Excellent")
 else:
         if grade >= 93.5 and grade <= 96.4:
-            print (1.25)
-            print ("Excellent")
+            print ("Your grade is: 1.25")
+            print ("Description: Excellent")
         else:
-            print ("Others")
+            if grade >=90.5 and grade <= 93.4:
+                print ("Your grade is: 1.5")
+                print ("Description: Very Good")
+            else:
+                if grade >= 87.5 and grade <= 90.4:
+                    print ("Your grade is: 1.75")
+                    print ("Description: Very Good")
+                else: 
+                    if grade >= 84.5 and grade <= 87.4:
+                        print ("Your grade is: 2.0")
+                        print ("Description: Good")
+                    else:
+                        print ("Others")
