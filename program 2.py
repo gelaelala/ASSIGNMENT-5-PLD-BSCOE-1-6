593f216a27ffa4b455575b0e905ad1a9a7e08c96
unknown <angelaescariocorpuz@gmail.com>
2021-11-16 12:46:05 +0800

    insertion of 1st if-else condition

--- program 2.py
+++ program 2.py
@@ -1,4 +1,16 @@
-firstnumber = int(input("Enter first number: "))
-secondnumber = int(input("Enter second number: "))
-thirdnumber = (input("Enter third number: "))
+def getnumbers ():
+    firstnumber = int(input("Enter first number: "))
+    secondnumber = int(input("Enter second number: "))
+    thirdnumber = (input("Enter third number: "))
+    return firstnumber, secondnumber, thirdnumber
 
+def lowestnumber (firstnum, secondnum, thirdnum):
+    if firstnum < secondnum:
+        if firstnum < thirdnum:
+            lowestnum = firstnum
+            print (firstnum,"is the smallest number.")
+        else:
+            lowestnum = thirdnum
+            print (thirdnum, "is the smallest number.")
+
+first_number, second_number, third_number = getnumbers()
